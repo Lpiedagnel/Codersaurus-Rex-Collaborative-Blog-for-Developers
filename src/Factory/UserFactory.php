32c97edeec341d81f9e4e99e865dcc6a252a6e47
Lpiedagnel <piedagnel.dev@gmail.com>
2023-06-05 11:48:53 +0200

    Create user Entity and add factory to generate users

---
+++ src/Factory/UserFactory.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+use Zenstruck\Foundry\RepositoryProxy;
+
+/**
+ * @extends ModelFactory<User>
+ *
+ * @method        User|Proxy create(array|callable $attributes = [])
+ * @method static User|Proxy createOne(array $attributes = [])
+ * @method static User|Proxy find(object|array|mixed $criteria)
+ * @method static User|Proxy findOrCreate(array $attributes)
+ * @method static User|Proxy first(string $sortedField = 'id')
+ * @method static User|Proxy last(string $sortedField = 'id')
+ * @method static User|Proxy random(array $attributes = [])
+ * @method static User|Proxy randomOrCreate(array $attributes = [])
+ * @method static UserRepository|RepositoryProxy repository()
+ * @method static User[]|Proxy[] all()
+ * @method static User[]|Proxy[] createMany(int $number, array|callable $attributes = [])
+ * @method static User[]|Proxy[] createSequence(iterable|callable $sequence)
+ * @method static User[]|Proxy[] findBy(array $attributes)
+ * @method static User[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method static User[]|Proxy[] randomSet(int $number, array $attributes = [])
+ */
+final class UserFactory extends ModelFactory
+{
+
+    const USERNAMES = [
+        "Dev_King",
+        "Not_A_Number",
+        "Hello_World",
+        "PHP_Lord",
+        "Echo",
+        "Console_log_addict",
+        "Extreme_React",
+        "JavaScript_Savior",
+        "Monthy_Python",
+        "TolkienDev",
+        "Jesus_C++",
+        "Java_Not_Script",
+        "Totally_a_real_person",
+        "Kira_Yoshikage",
+        "John_Doe",
+        "Code_Master"
+    ];
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher
+    )
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function getDefaults(): array
+    {
+        $username = self::faker()->randomElement(self::USERNAMES) . self::faker()->randomNumber(1, 50);
+        $domain = self::faker()->freeEmailDomain();
+        $email = $username . '@' . $domain;
+        
+        return [
+            'email' => $email,
+            'password' => 'test',
+            'roles' => [],
+            'username' => $username
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): self
+    {
+        return $this
+            ->afterInstantiate(function(User $user): void {
+                $user->setPassword($this->passwordHasher->hashPassword(
+                    $user,
+                    $user->getPassword()
+                ));
+            });
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return User::class;
+    }
+}
