ef35c12fa76c30df359a8ed6334e65826169d6e4
Lpiedagnel <piedagnel.dev@gmail.com>
2023-07-31 08:33:50 +0200

    Add Crud controller and add entry to dashboard. Has to customize

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -15,7 +19,7 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        return $this->render('admin/index.html.twig');
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
@@ -42,7 +46,11 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToUrl('Accueil', 'fas fa-home', $this->generateUrl('app_home'));
+        yield MenuItem::linkToCrud('Utilisateurs', 'fa fa-user-o', User::class);
+        yield MenuItem::linkToCrud('Articles', 'fa fa-list', Article::class);
+        yield MenuItem::linkToCrud('Categories', 'fa fa-list', Category::class);
+        yield MenuItem::linkToCrud('Commentaires', 'fa fa-folder-o', Comment::class);
+        yield MenuItem::linkToUrl('Se déconnecter', 'fas fa-undo', $this->generateUrl('app_logout'));
     }
 }
