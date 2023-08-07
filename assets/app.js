48858d8e483692800ea1cecc3710fe0cc814e1f2
Lpiedagnel <piedagnel.dev@gmail.com>
2023-08-07 15:50:58 +0200

    Add JS file to count the number of characters on Article Form

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,5 @@
 import './bootstrap.js';
+import './js/formCharacterCounter.js'
 /*
  * Welcome to your app's main JavaScript file!
  *
---
+++ assets/js/formCharacterCounter.js
@@ -0,0 +1,24 @@
+// This JS file is used on Article Form to help the user to know how many character he has left.
+
+/**
+ * Initialize the character counter for the field and add event listener.
+ *
+ * @param {string} inputId - ID of the field from the DOM.
+ * @param {number} maxLength - The maximum number of characters
+ */
+function initializeCharacterCounter(inputId, maxLength) {
+    const input = document.getElementById(inputId);
+    if (input) {
+      input.addEventListener('input', () => {
+        const remainingLength = maxLength - input.value.length;
+        input.nextElementSibling.textContent = remainingLength + ' caractères restants';
+      });
+    }
+  }
+  
+  // Initialize character counters for each field
+  initializeCharacterCounter('article_title', 100);
+  initializeCharacterCounter('article_extract', 100);
+  initializeCharacterCounter('article_meta_title', 60);
+  initializeCharacterCounter('article_meta_description', 160);
+  
